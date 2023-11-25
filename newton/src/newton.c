744cadc7de028992534e14866b539624e90e26da
PANAGIOTIS KOSTOPOYLOS <sdi2300100@linux10.di.uoa.gr>
2023-11-25 13:19:33 +0200

    i made a change to the program

--- newton/src/newton.c
+++ newton/src/newton.c
@@ -20,7 +20,7 @@ int main(int argc,char**argv) {
         printf("sorry but you have to check it again\n");
         return 1;
     }
-    double a0=(double)atoi(argv[1]),a1=(double)atoi(argv[2]), a2=(double)atoi(argv[3]),a3=(double)atoi(argv[4]),a4=(double)atoi(argv[5]),a5=(double)atoi(argv[6]),xo=(double)atoi(argv[7]);
+    double a0=strtod(argv[1],NULL),a1=strtod(argv[2],NULL),a2=strtod(argv[3],NULL),a3=strtod(argv[4],NULL),a4=strtod(argv[5],NULL),a5=strtod(argv[6],NULL),xo=strtod(argv[7],NULL);
     double x;
     x=xo-(factorial(a0,a1,a2,a3,a4,a5,xo)/factorial_p(a1,a2,a3,a4,a5,xo));
     int i=0;
